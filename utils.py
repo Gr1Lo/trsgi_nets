81d5d0e9be0ffc61a0cfcb9f332835a7218d7937
Grigroriy Lozhkin <lozhkin.grig@gmail.com>
2021-11-19 18:11:12 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -93,7 +93,7 @@ def read_pickle(f_path):
         df_test = pickle.load(f)
         return df_test
 
-def eof_an(df_clim_index, ds_n, n = 10, scale_type = 0):
+def eof_an(df_clim_index, ds_n, n = 10, scale_type = 0, pca_type = "varimax"):
 
     '''
     EOF-анализ
@@ -102,7 +102,7 @@ def eof_an(df_clim_index, ds_n, n = 10, scale_type = 0):
     scale_type - параметр, отвечающий за масштабирование при расчете EOF
     '''
 
-    pca = df_eof(df_clim_index,pca_type="varimax",n_components=n)
+    pca = df_eof(df_clim_index,pca_type=None,n_components=n)
     eofs = pca.eofs(s=2, n=n)
     eofs_da = eofs.stack(["latitude","longitude"]).to_xarray()
     pcs = pca.pcs(s=scale_type, n=n)
